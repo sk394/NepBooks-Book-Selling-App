a4be6454bee62a0c79c3c711e3fd6077131dd5bc
Suman Khadka <sk394@uakron.edu>
2023-06-14 14:03:56 -0400

    Add project files.

---
+++ SumanWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SumanWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
